ca0b9446567d7e95f7a0f135f2be80ab7ccf24b7
Web Hoan Hao <webhoanhao@gmail.com>
2017-09-12 12:35:17 +0700

    first commit

---
+++ src/SayHello.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace WebHoanHao\ReadDataHtmlTable;
+
+/**
+ * Description of SayHello
+ *
+ * @author BaoBui
+ */
+class SayHello {
+    public static function world() {
+        return 'Hello World, Composer!';
+    }
+}
